2f65a29c5a42968f53341702b86f3f30766c0520
italoHespanholPeres <pereshespanhol1@gmail.com>
2022-07-18 22:18:12 -0300

    Primeiro projeto java

---
+++ projeto-git/src/exemploGit/OlaMundo.java
@@ -0,0 +1,18 @@
+package exemploGit;
+
+public class OlaMundo {
+	public static void main(String[] args) {
+		
+		String descricao = "Olá mundo";
+		
+		System.out.println(descricao);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
